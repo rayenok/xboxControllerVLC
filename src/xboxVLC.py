22f5b2951b6d7595680645abdf62ec501935654d
Eric Nova <sk0l3m@gmail.com>
2014-02-15 18:59:41 +0100

    Added the operations of Play next, Play previous, increase volume, decrease volum and Pause/Play

--- src/xboxVLC.py
+++ src/xboxVLC.py
@@ -1,10 +1,10 @@
 
-import logging
-import pygame
+#!/usr/bin/python2.7
+import logging,os,time,pygame
 from pygame.locals import *
 
 
-def buttonsController(nButton, value):
+def buttonsController(nButton):
     # A: 0
     # B: 1
     # X: 2
@@ -16,38 +16,30 @@ def buttonsController(nButton, value):
     # start: 7
     if nButton == 0:
         # A controller -> reduce the volume
-        print "A"
-        pass
+        os.system('amixer -q set Master 5%-')
     elif nButton == 1:
         # B controller -> play next
-        print "B"
-        pass
+        os.system('dbus-send --session --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next')
     elif nButton == 2:
         # X controller -> play previous
-        print "X"
-        pass
+        os.system('dbus-send --session --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous')
     elif nButton == 3:
         # Y controller -> increase the volume
-        print "Y"
-        pass
+        os.system('amixer -q set Master 5%+')
+    elif nButton == 7:
+        # Y controller -> increase the volume
+        os.system('dbus-send --session --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause')
 
 
 pygame.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
-
+time.sleep(5)
 while 1:
-        # clock.tick(60)
     try:
         for event in pygame.event.get():
                 if event.type == JOYBUTTONDOWN:
                     # logging.log(10,"Down: "+str(event.button))
-                    buttonsController(event.button,1)
-                elif event.type == JOYBUTTONUP:
-                    # logging.log(10,"Up: "+str(event.button))
-                    # print "Up: "+str(event.button)
-                    buttonsController(event.button,0)
+                    buttonsController(event.button)
     except:
         pass
-
-# pythoncom.PumpMessages()
