4397809efa61c69f08686717e64a21ef66a1d217
Eric Nova <ericnova3@gmail.com>
2014-02-24 06:21:42 +0100

    Changed system calls for subprocess and fixed a problem with the initialization of the joystick

--- src/xboxVLC.py
+++ src/xboxVLC.py
@@ -1,6 +1,8 @@
-
 #!/usr/bin/python2.7
-import logging,os,time,pygame
+import os
+import time
+import pygame
+import subprocess
 from pygame.locals import *
 
 
@@ -14,32 +16,68 @@ def buttonsController(nButton):
     # RB: 5
     # back: 6
     # start: 7
-    if nButton == 0:
-        # A controller -> reduce the volume
-        os.system('amixer -q set Master 5%-')
-    elif nButton == 1:
-        # B controller -> play next
-        os.system('dbus-send --session --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next')
-    elif nButton == 2:
-        # X controller -> play previous
-        os.system('dbus-send --session --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous')
-    elif nButton == 3:
-        # Y controller -> increase the volume
-        os.system('amixer -q set Master 5%+')
-    elif nButton == 7:
-        # Y controller -> increase the volume
-        os.system('dbus-send --session --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause')
 
+    try:
+        if nButton == 0:
+            # A controller -> reduce the volume
+            os.system('amixer -q set Master 5%-')
+        elif nButton == 1:
+            # B controller -> play next
+            with open(os.devnull, 'wb') as devnull:
+                subprocess.check_call(['dbus-send',
+                                       '--session',
+                                       '--type=method_call',
+                                       '--print-reply',
+                                       '--dest=org.mpris.MediaPlayer2.vlc',
+                                       '/org/mpris/MediaPlayer2',
+                                       'org.mpris.MediaPlayer2.Player.Next'],
+                                       stdout=devnull,
+                                       stderr=devnull)
+        elif nButton == 2:
+            # X controller -> play previous
+            with open(os.devnull, 'wb') as devnull:
+                subprocess.check_call(['dbus-send',
+                                        '--session',
+                                        '--type=method_call',
+                                        '--print-reply',
+                                        '--dest=org.mpris.MediaPlayer2.vlc',
+                                        '/org/mpris/MediaPlayer2',
+                                        'org.mpris.MediaPlayer2.Player.Previous'],
+                                        stdout=devnull,
+                                        stderr=devnull)
+        elif nButton == 3:
+            # Y controller -> increase the volume
+            os.system('amixer -q set Master 5%+')
+        elif nButton == 7:
+            # Y controller -> increase the volume
+            with open(os.devnull, 'wb') as devnull:
+                print "start"
+                subprocess.check_call(['dbus-send',
+                                        '--session',
+                                        '--type=method_call',
+                                        '--print-reply',
+                                        '--dest=org.mpris.MediaPlayer2.vlc',
+                                        '/org/mpris/MediaPlayer2',
+                                        'org.mpris.MediaPlayer2.Player.PlayPause'],
+                                        stdout=devnull,
+                                        stderr=devnull)
+    except:
+        pass
 
+# pygame.init()
+# joystick = pygame.joystick.Joystick(0)
+# joystick.init()
+# time.sleep(5)
 pygame.init()
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
-time.sleep(5)
 while 1:
     try:
-        for event in pygame.event.get():
-                if event.type == JOYBUTTONDOWN:
-                    # logging.log(10,"Down: "+str(event.button))
-                    buttonsController(event.button)
+        joystick_count = pygame.joystick.get_count()
+        for i in range(joystick_count):
+            joystick = pygame.joystick.Joystick(0)
+            joystick.init()
+
+            for event in pygame.event.get():
+                    if event.type == JOYBUTTONDOWN:
+                        buttonsController(event.button)
     except:
         pass
